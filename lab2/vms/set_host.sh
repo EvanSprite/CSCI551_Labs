24c30e1f8496a6fa926e607b2be0f61b644c0276
evansprite <xswyfxs@gmail.com>
2017-10-05 20:32:27 -0700

    Lab 2 submission

---
+++ lab2/vms/set_host.sh
@@ -0,0 +1,18 @@
+sudo ./go_to.sh NEWY-host ifconfig newy 11.101.0.1/24
+sudo ./go_to.sh NEWY-host route add default gw 11.101.0.2
+sudo ./go_to.sh CHIC-host ifconfig chic 11.102.0.1/24
+sudo ./go_to.sh CHIC-host route add default gw 11.102.0.2
+sudo ./go_to.sh WASH-host ifconfig  wash 11.103.0.1/24
+sudo ./go_to.sh WASH-host route add default gw 11.103.0.2
+sudo ./go_to.sh ATLA-host ifconfig atla 11.104.0.1/24
+sudo ./go_to.sh ATLA-host route add default gw 11.104.0.2
+sudo ./go_to.sh KANS-host ifconfig kans 11.105.0.1/24
+sudo ./go_to.sh KANS-host route add default gw 11.105.0.2
+sudo ./go_to.sh HOUS-host ifconfig hous 11.106.0.1/24
+sudo ./go_to.sh HOUS-host route add default gw 11.106.0.2
+sudo ./go_to.sh SALT-host ifconfig salt 11.107.0.1/24
+sudo ./go_to.sh SALT-host route add default gw 11.107.0.2
+sudo ./go_to.sh LOSA-host ifconfig losa 11.108.0.1/24
+sudo ./go_to.sh LOSA-host route add default gw 11.108.0.2
+sudo ./go_to.sh SEAT-host ifconfig seat 11.109.0.1/24
+sudo ./go_to.sh SEAT-host route add default gw 11.109.0.2
