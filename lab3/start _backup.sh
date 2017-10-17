991cd614fdcc3bfbd6cd55659d61184e5f4ef47d
evansprite <xswyfxs@gmail.com>
2017-10-17 16:55:42 -0700

    Lab 3 submission

---
+++ lab3/start _backup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/home/cs551/evansprite-cs551/lab1/router/run_pox.sh 
+sudo /home/cs551/evansprite-cs551/lab2/miniNExT/start_internet2.py 2 /home/cs551/evansprite-cs551/lab2/miniNExT/groups/ips.group --new
+
